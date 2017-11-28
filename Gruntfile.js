b86f6b5de40ed54b1848703993d80be3667f151a
Matt Lawson <matt@lawlesscreation.com>
2017-11-28 19:43:53 +0000

    demo files

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+
+        config: {
+            src: './src',
+            dist: './dist'
+        },
+
+        // Builder
+        assemble: {
+            options: {
+                partials: ['<%= config.src %>/partials/*.hbs'],
+                flatten: true
+            },
+            patterns: {
+                options: {
+                    layout: '<%= config.src %>/layouts/patterns.hbs'
+                },
+                files: {'<%= config.dist %>/': ['<%= config.src %>/patterns/**/*.hbs']}
+            }
+        },
+
+        // Clean the dist folder
+        clean: {
+            all: ['<%= config.dist %>/']
+        },
+
+        watch: {
+            html: {
+                files: [
+                    '<%= config.src %>/layouts/**/*.hbs',
+                    '<%= config.src %>/patterns/**/*.{html,hbs,json}'
+                ],
+                tasks: ['assemble:patterns']
+            }
+        },
+
+        // Static file server
+        browserSync: {
+            dev: {
+                options: {
+                    directory: true,
+                    server: '<%= config.dist %>',
+                    watchTask: true
+                },
+                bsFiles: {
+                    src : [
+                        '<%= config.dist %>/**/*'
+                    ]
+                }
+            }
+        }
+    });
+  
+    grunt.loadNpmTasks('grunt-assemble');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // The default task to run with the `grunt` command.
+    grunt.registerTask('default', ['clean', 'assemble', 'browserSync', 'watch']);
+
+    // The build task to run with the `grunt` command.
+    grunt.registerTask('build', ['clean', 'assemble']);
+  };
